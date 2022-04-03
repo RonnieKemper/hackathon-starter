5e10baacaf6ed3b2beae3af517cb68cbc04b85ad
RonaldKemper <ronspoolco@gmail.com>
2022-04-03 02:59:48 -0400

    'revision'

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: 'ec2-3-80-54-171.compute-1.amazonaws.com',
       username: 'ubuntu',
-      privateKey: '../DeploymentIntroPublicKey.pem'
+      privateKey: 'C:\Users\ronsp\OneDrive\Desktop\ID\DEV\SSH_Keys\labsuser.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
